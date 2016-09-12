a85632adedd19feaf4e0a1f0039e30690a24c7dc
Chandler Prall <chandler@sympoz.com>
2016-09-12 16:22:47 -0600

    card styles

--- src/views/HomepageView.js
+++ src/views/HomepageView.js
@@ -6,28 +6,92 @@ import Paragraph from '../components/Paragraph';
 export default function HomepageView() {
     return (
         <div className="centered">
-			<div className="row">
-				<section className="column-major">
+			<div className="card">
+				<section className="cardTitle">
 					<Headline>Goblin Physics</Headline>
+				</section>
+				<section className="cardContent">
 					<Paragraph>Goblin Physics is an open source physics engine written from the ground up for JavaScript games and applications. Its goal is to provide fast and dependable physics simulation regardless of platform (desktop or mobile, browser or nodejs).</Paragraph>
 					<Paragraph><Link href="http://www.goblinphysics.com" className="sectionLink">http://www.goblinphysics.com</Link></Paragraph>
 				</section>
-				<section className="column-minor">
+				<section className="cardImage">
 					<Link href="http://www.goblinphysics.com" className="sectionLink">
 						<img src="/static/images/goblinphysics.jpg" alt="Goblin Physics"/>
 					</Link>
 				</section>
 			</div>
 
-			<div className="row">
-				<section className="column-major">
+			<div className="card">
+				<section className="cardTitle">
 					<Headline>Physijs</Headline>
+				</section>
+				<section className="cardContent">
 					<Paragraph>A physics plugin extending the Three.js API with a customizable, easy to use physics engine. The simulation is run via web workers to keep processing off the main thread.</Paragraph>
 					<Paragraph><Link href="http://chandlerprall.github.io/Physijs/" className="sectionLink">http://chandlerprall.github.io/Physijs/</Link></Paragraph>
 				</section>
-				<section className="column-minor">
+				<section className="cardImage">
 					<Link href="http://chandlerprall.github.io/Physijs/" className="sectionLink">
 						<img src="/static/images/physijs.jpg" alt="Physijs"/>
+					</Link>
+				</section>
+			</div>
+	
+			<div className="card">
+				<section className="cardTitle">
+					<Headline>A</Headline>
+				</section>
+				<section className="cardContent">
+					<Paragraph>Goblin Physics is an open source physics engine written from the ground up for JavaScript games and applications. Its goal is to provide fast and dependable physics simulation regardless of platform (desktop or mobile, browser or nodejs).</Paragraph>
+					<Paragraph><Link href="http://www.goblinphysics.com" className="sectionLink">http://www.goblinphysics.com</Link></Paragraph>
+				</section>
+				<section className="cardImage">
+					<Link href="http://www.goblinphysics.com" className="sectionLink">
+						<img src="/static/images/goblinphysics.jpg" alt="Goblin Physics"/>
+					</Link>
+				</section>
+			</div>
+	
+			<div className="card">
+				<section className="cardTitle">
+					<Headline>B</Headline>
+				</section>
+				<section className="cardContent">
+					<Paragraph>Goblin Physics is an open source physics engine written from the ground up for JavaScript games and applications. Its goal is to provide fast and dependable physics simulation regardless of platform (desktop or mobile, browser or nodejs).</Paragraph>
+					<Paragraph><Link href="http://www.goblinphysics.com" className="sectionLink">http://www.goblinphysics.com</Link></Paragraph>
+				</section>
+				<section className="cardImage">
+					<Link href="http://www.goblinphysics.com" className="sectionLink">
+						<img src="/static/images/goblinphysics.jpg" alt="Goblin Physics"/>
+					</Link>
+				</section>
+			</div>
+
+			<div className="card">
+				<section className="cardTitle">
+					<Headline>C</Headline>
+				</section>
+				<section className="cardContent">
+					<Paragraph>Goblin Physics is an open source physics engine written from the ground up for JavaScript games and applications. Its goal is to provide fast and dependable physics simulation regardless of platform (desktop or mobile, browser or nodejs).</Paragraph>
+					<Paragraph><Link href="http://www.goblinphysics.com" className="sectionLink">http://www.goblinphysics.com</Link></Paragraph>
+				</section>
+				<section className="cardImage">
+					<Link href="http://www.goblinphysics.com" className="sectionLink">
+						<img src="/static/images/goblinphysics.jpg" alt="Goblin Physics"/>
+					</Link>
+				</section>
+			</div>
+
+			<div className="card">
+				<section className="cardTitle">
+					<Headline>D</Headline>
+				</section>
+				<section className="cardContent">
+					<Paragraph>Goblin Physics is an open source physics engine written from the ground up for JavaScript games and applications. Its goal is to provide fast and dependable physics simulation regardless of platform (desktop or mobile, browser or nodejs).</Paragraph>
+					<Paragraph><Link href="http://www.goblinphysics.com" className="sectionLink">http://www.goblinphysics.com</Link></Paragraph>
+				</section>
+				<section className="cardImage">
+					<Link href="http://www.goblinphysics.com" className="sectionLink">
+						<img src="/static/images/goblinphysics.jpg" alt="Goblin Physics"/>
 					</Link>
 				</section>
 			</div>
