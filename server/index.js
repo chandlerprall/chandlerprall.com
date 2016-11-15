1fb739de8bcfc1ca6bd31bc50f4be1b2716e765e
Chandler Prall <chandler@sympoz.com>
2016-11-14 21:59:52 -0700

    set cache control max age for static files

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const React = require('react');
 const ReactDOM = require('react-dom/server');
 const server = express();
 
-server.use(`/static/${app.BUNDLE}/`, express.static(`${__dirname}/../dist/web`));
-server.use('/static/', express.static(`${__dirname}/static`));
+server.use(`/static/${app.BUNDLE}/`, express.static(`${__dirname}/../dist/web`, {maxAge: 7 * 24 * 60 * 60 * 1000})); // cache for 7 days
+server.use('/static/', express.static(`${__dirname}/static`, {maxAge: 1 * 60 * 60 * 1000})); // cache for 1 hour
 
 server.use((req, res, next) => {
 	app.router.route(
